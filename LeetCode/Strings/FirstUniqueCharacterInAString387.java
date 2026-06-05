ced95dafef243a58f3f33e102d63dbb8a8fbaa5b
decypher0 <gauravhabad113@gmail.com>
2026-06-06 02:51:18 +0530

    FirstUniqueCharacterInAString387 - 06-06-2026

--- LeetCode/Strings/FirstUniqueCharacterInAString387.java
+++ LeetCode/Strings/FirstUniqueCharacterInAString387.java
@@ -1,5 +1,10 @@
 package LeetCode.Strings;
 
+import javax.swing.text.html.parser.Entity;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class FirstUniqueCharacterInAString387 {
 //    Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 //
@@ -26,28 +31,38 @@ public class FirstUniqueCharacterInAString387 {
 //    Input: s = "aabb"
 //
 //    Output: -1
-//
-//
-//
 //    Constraints:
-//
 //            1 <= s.length <= 105
 //    s consists of only lowercase English letters.
     public int firstUniqueCharacterInAString(String s){
-        char a = s.charAt(0);
-        int b = 0;
-        for(int i = b; i<s.length();i++){
-            if(a==s.charAt(i)){
-                a = s.charAt(i);
-                b++;
+        int res = -1;
+        LinkedHashMap<Character, Integer> charTable = new LinkedHashMap<>();
+        for(int i = 0; i<s.length();i++){
+            if(!charTable.containsKey(s.charAt(i))){
+                charTable.put(s.charAt(i), 1);
+            }else{
+                charTable.put(s.charAt(i), charTable.get(s.charAt(i))+1);
             }
         }
-        return b >= 0 ? b : -1;
+        char resCharV = 0;
+        if(charTable.containsValue(1)){
+            for(Map.Entry<Character, Integer> c : charTable.entrySet()){
+                if(c.getValue() == 1) {
+                    resCharV = c.getKey();
+                    break;
+                }
+            }
+        }
+        for(int i = 0;i<s.length();i++){
+            if(resCharV==s.charAt(i))
+                res = i;
+        }
+        return res;
     }
 
     public static void main(String[] args) {
         FirstUniqueCharacterInAString387 solver = new FirstUniqueCharacterInAString387();
-        System.out.println(solver.firstUniqueCharacterInAString("leetcode"));
+        System.out.println(solver.firstUniqueCharacterInAString("dabbcb"));
 
     }
 }
